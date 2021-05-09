7f162b393d78346c9e8a76f3f5ebdc1e9b8d678f
jsato <jsato@yahoo-corp.jp>
2021-05-09 23:52:15 +0900

    unicafe step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -17,15 +17,14 @@ const Statistics = ({good, neutral, bad}) => {
   const average = all ? (good - bad) / all : 0
   const positive = all ? good / all * 100 : 0
   return (
-    <>
-      <h1>statistics</h1>
+    <div>
       <Display text="good" value={good} />
       <Display text="neutral" value={neutral} />
       <Display text="bad" value={bad} />
       <Display text="all" value={all} />
       <Display text="average" value={average} />
       <Display text="positive" value={positive} unit="%" />
-    </>
+    </div>
   )
 }
 
@@ -35,15 +34,29 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>
-  )
+  if (good + neutral + bad === 0) {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+        <h1>statistics</h1>
+        <div>no feedback given</div>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+        <h1>statistics</h1>
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      </div>
+    )
+  }
 }
 
 export default App
