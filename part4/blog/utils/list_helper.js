2f00a2dcde66cf8d6cbb12b6f04d410058d42132
jsato <jsato@yahoo-corp.jp>
2021-06-27 18:31:12 +0900

    4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length == 0) {
+    return []
+  } else {
+    const blogWithMaxLikes = blogs.reduce((x, y) => x.likes < y.likes ? y : x)
+    return {
+      title: blogWithMaxLikes.title,
+      author: blogWithMaxLikes.author,
+      likes: blogWithMaxLikes.likes
+    }
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
