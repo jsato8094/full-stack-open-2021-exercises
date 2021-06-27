7f171c03b410534f0e62ffa01705b7f8ffc21336
jsato <jsato@yahoo-corp.jp>
2021-06-27 20:05:18 +0900

    4.8

---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  for (let blog of initialBlogs) {
+    let obj = new Blog(blog)
+    await obj.save()
+  }
+})
+
+describe('GET /api/blogs', () => {
+  test('blogs are returned as json', async () => {
+    await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
